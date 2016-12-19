c13cb66a724546e54464771f0986959f87fff31b
Robert Utterback <robertu13@gmail.com>
2016-12-19 15:12:50 -0600

    Adding config makefile

---
+++ config.mk
@@ -0,0 +1,8 @@
+PROJECT_HOME = $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
+BUILD_DIR = $(PROJECT_HOME)/build
+COMPILER_HOME = $(PROJECT_HOME)/llvm-cilk
+RUNTIME_LIB = $(COMPILER_HOME)/lib/x86_64/libcilkrts.a
+
+OPT_FLAGS = -O0
+TOOL_DEBUG = 1
+LTO = 1
