639254bb858886b5c4f0248197ff5de2c66216fd
Robert Utterback <robertu13@gmail.com>
2017-01-06 15:11:45 -0600

    Cleanup for tool source code. Just prints out each function for now

--- src/tool.cpp
+++ src/tool.cpp
@@ -1,32 +1,81 @@
+#include "tsan.hpp"
+#include "debug.hpp"
+
+#include <internal/abi.h>
+
+#include <cstdio>
+#include <cassert>
+
+
+#define TOOL_ASSERT(x) assert(x) /// @TODO{refactor asserts}
+
+extern "C" {
+
+// XXX: namespace?
+void print_stats(FILE *output = stdout) {
+  fprintf(output, "----- Cilk Tool Stats -----\n");
+}
+
 void cilk_tool_init(void) { }
 void cilk_tool_destroy(void) { }
 
 void cilk_enter_begin() {
-  disable_checking(); 
-  INCR(num_cilk_functions);
+  DBG_TRACE();
+  disable_checking();
 }
-void cilk_enter_helper_begin(__cilkrts_stack_frame* sf, void* this_fn, void* rip) { 
+void cilk_enter_helper_begin(__cilkrts_stack_frame* sf,
+                             void* this_fn, void* rip) {
+  DBG_TRACE();
   disable_checking(); 
-  INCR(num_spawns);
 }
-void cilk_enter_end(__cilkrts_stack_frame *sf, void *rsp) { enable_checking(); }
+
+void cilk_enter_end(__cilkrts_stack_frame *sf, void *rsp) {
+  DBG_TRACE();
+  enable_checking();
+}
 
 void cilk_sync_begin(__cilkrts_stack_frame* sf) {
+  DBG_TRACE();
   disable_checking();
-  INCR(num_syncs);
 }
-void cilk_sync_end() { enable_checking(); }
-void cilk_resume(__cilkrts_stack_frame* sf) { enable_checking(); }
+void cilk_sync_end() {
+  DBG_TRACE();
+  enable_checking();
+}
+void cilk_resume(__cilkrts_stack_frame* sf) {
+  DBG_TRACE();
+  enable_checking();
+}
 
-void cilk_spawn_prepare() { disable_checking(); }
-void cilk_spawn_or_continue(int in_continuation) { enable_checking(); }
+void cilk_spawn_prepare() {
+  DBG_TRACE();
+  disable_checking();
+}
+void cilk_spawn_or_continue(int in_continuation) {
+  DBG_TRACE();
+  enable_checking();
+}
 //void cilk_continue(__cilkrts_stack_frame* sf, char* new_sp) { }
 
-void cilk_detach_begin(__cilkrts_stack_frame* sf, void* this_fn, void* rip) { disable_checking(); }
-void cilk_detach_end() { enable_checking(); }
+void cilk_detach_begin(__cilkrts_stack_frame* sf,
+                       void* this_fn, void* rip) {
 
-void cilk_leave_begin(__cilkrts_stack_frame* sf) { disable_checking(); }
-void cilk_leave_end() { enable_checking(); }
+  DBG_TRACE();
+  disable_checking();
+}
+void cilk_detach_end() {
+  DBG_TRACE();
+  enable_checking();
+}
+
+void cilk_leave_begin(__cilkrts_stack_frame* sf) {
+  DBG_TRACE();
+  disable_checking();
+}
+void cilk_leave_end() {
+  DBG_TRACE();
+  enable_checking();
+}
 
 void cilk_steal_success(__cilkrts_worker *w, __cilkrts_worker *victim,
 			__cilkrts_stack_frame *sf) {
@@ -34,3 +83,5 @@ void cilk_steal_success(__cilkrts_worker *w, __cilkrts_worker *victim,
   // We don't need to enable checking here b/c this worker will
   // immediately execute cilk_spawn_or_continue
 }
+
+} // extern "C"
