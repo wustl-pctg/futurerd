03bc445643f100cf2ffc17f01584f4297bb79429
Robert Utterback <robertu13@gmail.com>
2017-01-08 19:16:04 -0600

    Fixed makefile for tests

--- test/test.mk
+++ test/test.mk
@@ -1,5 +1,13 @@
 TEST_DIR:=$(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))
 include $(TEST_DIR)/../common.mk
 
-APP_TOOL_ONLY_FLAGS = -fcilktool
-APP_RD_FLAGS = $(APP_TOOL_ONLY_FLAGS) -fsanitize=thread
+# APP_TOOL_ONLY_FLAGS = -fcilktool
+# APP_RD_FLAGS = $(APP_TOOL_ONLY_FLAGS) -fsanitize=thread
+
+# Why doesn't clang pick this up automatically?
+INC += -I$(PROJECT_HOME)/llvm-cilk/include -I$(PROJECT_HOME)/src/
+APPFLAGS = -fcilkplus -fcilktool -fsanitize=thread
+
+CFLAGS += $(APPFLAGS)
+CXXFLAGS += $(APPFLAGS)
+LDFLAGS += -ldl -lpthread $(RUNTIME_LIB)
