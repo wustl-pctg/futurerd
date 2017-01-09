14ae41b93208feb81a91740b9be28279972f124b
Robert Utterback <robertu13@gmail.com>
2017-01-09 12:50:01 -0600

    Adding test 6

---
+++ test/basic/test6.cpp
@@ -0,0 +1,34 @@
+// Access to shared variable -- race
+#include <iostream>
+#include <cassert>
+
+#include <cilk/cilk.h>
+#define spawn cilk_spawn
+#define sync cilk_sync
+#include <future.hpp>
+#include <futurerd.hpp>
+
+void foo(cilk::future<int>& f, int& shared) {
+  f.put(42);
+  shared = 57;
+}
+
+int bar(cilk::future<int>& f, int& shared) {
+  int x = f.get();
+  return shared - x;
+}
+
+int main(int argc, char* argv[])
+{
+  futurerd::set_policy(futurerd::CONTINUE);
+  int shared = 0;
+  cilk::future<int> f; f.start();
+  spawn foo(f, shared);
+  int x = bar(f, shared);
+  sync;
+
+  assert(x == 15 || x == -42);
+  assert(futurerd::num_races() == 1);
+  
+  return 0;
+}
