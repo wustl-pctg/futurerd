39173ca780da2b17f31a5d1b2ce29fc4c6dcc443
Robert Utterback <robertu13@gmail.com>
2017-01-09 12:49:57 -0600

    Adding test 4

---
+++ test/basic/test4.cpp
@@ -0,0 +1,37 @@
+// Access to global variable -- race
+#include <iostream>
+#include <cassert>
+
+#include <cilk/cilk.h>
+#define spawn cilk_spawn
+#define sync cilk_sync
+#include <future.hpp>
+#include <futurerd.hpp>
+
+int g_shared = 0;
+
+void foo(cilk::future<int>& f) {
+  f.put(42);
+  g_shared = 57;
+}
+
+int bar(cilk::future<int>& f) {
+  int x = f.get();
+  return g_shared - x;
+}
+
+
+int main(int argc, char* argv[])
+{
+  futurerd::set_policy(futurerd::CONTINUE);
+  cilk::future<int> f;
+  f.start();
+  spawn foo(f);
+  int x = bar(f);
+  sync;
+
+  assert(x == 15 || x == -42);
+  assert(futurerd::num_races() == 1);
+
+  return 0;
+}
