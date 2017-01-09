25cb9a63e79991343e0389931a5f0bdc9d333272
Robert Utterback <robertu13@gmail.com>
2017-01-09 12:50:31 -0600

    Adding debug helper module

---
+++ src/debug.cpp
@@ -0,0 +1,39 @@
+#include <cstdarg> // va_list
+#include <cstdlib> // exit
+
+#include "debug.hpp"
+
+using namespace futurerd;
+using namespace debug;
+
+namespace futurerd {
+
+namespace debug {
+
+void printf(DebugLevel level, const char *fmt, ...) {
+    if(g_level & level) {
+        std::va_list l;
+        va_start(l, fmt);
+        std::vfprintf(stderr, fmt, l);
+        va_end(l);
+    }
+}
+
+// Print out the error message and exit
+[[noreturn]] void die(const char *fmt, ...) {
+
+    std::va_list l;
+    std::fprintf(stderr, "=================================================\n");
+    std::fprintf(stderr, "racedetector: fatal error\n");
+
+    va_start(l, fmt);
+    std::vfprintf(stderr, fmt, l);
+    std::fprintf(stderr, "=================================================\n");
+    fflush(stderr);
+    va_end(l);
+    std::exit(1);
+}
+
+} // namespace debug
+
+} // namespace futurerd
---
+++ src/debug.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <cassert>
+#include <cstdio>
+
+#ifndef DNDEBUG
+#define DBG_TRACE(fmt, args...)                                      \
+  futurerd::debug::printf(futurerd::debug::TRACE,                    \
+                          "[%s]: " fmt, __func__, ## args)
+#else
+#define DBG_TRACE
+#endif
+
+namespace futurerd {
+
+namespace debug {
+
+enum DebugLevel : int {
+  NONE = 0,
+  BASIC = 1,
+  TRACE = 2,
+  CALLBACK = 4,
+  MEMORY = 8
+};
+
+static DebugLevel g_level = futurerd::debug::BASIC;
+
+[[noreturn]] void die(const char *fmt, ...);
+void printf(DebugLevel level, const char *fmt, ...);
+
+} // namespace debug
+
+} // namespace futurerd
