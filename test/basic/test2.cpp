e7cb904152996a6dce64656682efd183d6905941
Robert Utterback <robertu13@gmail.com>
2017-01-09 12:49:53 -0600

    Adding test 2

---
+++ test/basic/test2.cpp
@@ -0,0 +1,28 @@
+// Very simple use of futures
+#include <iostream>
+#include <cassert>
+
+#include <cilk/cilk.h>
+#define spawn cilk_spawn
+#define sync cilk_sync
+#include <future.hpp>
+#include <futurerd.hpp>
+
+void foo(cilk::future<int>& f) { f.put(42); }
+int bar(cilk::future<int>& f) { return f.get(); }
+
+int main(int argc, char* argv[])
+{
+  futurerd::set_policy(futurerd::CONTINUE);
+
+  cilk::future<int> f;
+  f.start();
+  spawn foo(f);
+  int x = bar(f);
+  sync;
+
+  assert(x == 42);
+  assert(futurerd::num_races() == 0);
+
+  return 0;
+}
