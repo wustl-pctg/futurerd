b99708fcad14af140f1bd53d41b0bdd458cc4066
Robert Utterback <robertu13@gmail.com>
2017-01-16 15:24:37 -0600

    Minor update to unionfind

--- src/unionfind.hpp
+++ src/unionfind.hpp
@@ -3,13 +3,15 @@
 namespace uf {
 struct node {
   static std::size_t global_index;
-  //static ChunkedList<node> allocator;
   static void* operator new(std::size_t sz);
   static void* operator new[](std::size_t sz) = delete;
 
   std::size_t id;
   std::size_t size;
   node* parent = nullptr;
+
+  // Field for "attached" and "done"?
+  
   node() : id(global_index++) {}
 }; // struct node
 
