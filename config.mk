9d06403bf469b1ae6f1689f5990e2ef380c216c6
Robert Utterback <robertu13@gmail.com>
2017-01-27 17:00:10 -0600

    minor

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 PROJECT_HOME = $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
 BUILD_DIR = $(PROJECT_HOME)/build
 COMPILER_HOME = $(PROJECT_HOME)/llvm-cilk
-RUNTIME_LIB = $(COMPILER_HOME)/lib/x86_64/libcilkrts.a
+RUNTIME_LIB = $(COMPILER_HOME)/lib/libcilkrts.a
 
 OPT_FLAGS = -O0
 TOOL_DEBUG = 1
