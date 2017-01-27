07eeb2322c48c5b4066968bb8f88be9a74f6d2e2
Robert Utterback <robertu13@gmail.com>
2017-01-27 17:01:51 -0600

    some shadow stack code

--- src/tool.cpp
+++ src/tool.cpp
@@ -1,16 +1,27 @@
 #include "tsan.hpp"
 #include "debug.hpp"
 #include "unionfind.hpp"
+#include "stack.hpp"
 
 #include <internal/abi.h>
 
 #include <cstdio>
 #include <cassert>
 
-
 #define TOOL_ASSERT(x) assert(x) /// @TODO{refactor asserts}
 
+struct FrameData {
+  int placeholder;
+}; // struct FrameData
+
+// This won't work for parallel detection, since we need to access
+// other workers' shadow stacks
+__thread Stack<FrameData> t_sstack;
+
 extern "C" {
+
+// First cilk frame or steal
+void init_strand() {}
 
 void cilk_tool_init(void) { }
 void cilk_tool_destroy(void) { }
@@ -18,15 +29,25 @@ void cilk_tool_destroy(void) { }
 void cilk_enter_begin() {
   DBG_TRACE();
   disable_checking();
+  t_sstack.push();
 }
 void cilk_enter_helper_begin(__cilkrts_stack_frame* sf,
                              void* this_fn, void* rip) {
   DBG_TRACE();
-  disable_checking(); 
+  disable_checking();
 }
 
 void cilk_enter_end(__cilkrts_stack_frame *sf, void *rsp) {
   DBG_TRACE();
+
+  if (t_sstack.empty()) {
+    init_strand();
+    // if (!check_enable_instrumentation) {
+    //   check_enable_instrumentation = false;
+    //   enable_instrumentation();
+    //   stack_low_watermark = (uint64_t)(-1);
+    // }
+  }
   enable_checking();
 }
 
@@ -58,6 +79,8 @@ void cilk_detach_begin(__cilkrts_stack_frame* sf,
 
   DBG_TRACE();
   disable_checking();
+
+  t_sstack.push_helper();
 }
 void cilk_detach_end() {
   DBG_TRACE();
