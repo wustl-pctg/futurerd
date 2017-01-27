ba3a9563779d6e26a451bc96e2d16dd64c7bdb3f
Robert Utterback <robertu13@gmail.com>
2017-01-27 17:01:05 -0600

    Minor

--- src/futurerd.cpp
+++ src/futurerd.cpp
@@ -6,8 +6,12 @@
 
 namespace futurerd {
 
+typedef uf::node set_t;
+
+/// XXX: Should be detector state object
 static DetectPolicy g_policy = futurerd::DetectPolicy::ABORT;
 static size_t g_num_races = 0;
+__thread set_t current;
 
 void set_policy(DetectPolicy p) {
   if (p > 1) debug::die("Invalid Detection Policy\n");
