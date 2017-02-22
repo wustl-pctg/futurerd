b5faf371c81d109d589359abfefdc8f10ab593b6
Robert Utterback <robertu13@gmail.com>
2017-02-22 16:43:34 -0600

    Lots of makefile changes

--- Makefile
+++ Makefile
@@ -2,8 +2,14 @@ include common.mk
 
 TARGET = $(LIB_DIR)/libfuturerd.a
 
-default: 
+.DEFAULT_GOAL := lib
+
+lib:
 	$(MAKE) -C ./src
 
 check: $(TARGET)
 	$(MAKE) -C ./test/basic check
+
+clean:
+	$(MAKE) -C ./src clean
+	$(MAKE) -C ./test/basic clean
--- common.mk
+++ common.mk
@@ -7,29 +7,46 @@ LIB_DIR = $(BUILD_DIR)/lib
 CC = $(COMPILER_HOME)/bin/clang
 CXX = $(COMPILER_HOME)/bin/clang++
 
-OPT_FLAGS ?= -O3
+OPT_FLAGS = -O3
+DBG_FLAGS = -O0
+PROF_FLAGS = -O2
 TOOL_DEBUG ?= 0
-LTO ?= 1
 
-INC = -I$(PROJECT_HOME)/include 
+ifeq ($(mode),release)
+	EXTRA_FLAGS += $(OPT_FLAGS)
+else ifeq ($(mode),profile)
+  EXTRA_FLAGS += $(PROF_FLAGS)
+else ifeq ($(mode),debug)
+  EXTRA_FLAGS += $(DBG_FLAGS)
+else ifeq ($(mode),) # default value
+  EXTRA_FLAGS += $(DBG_FLAGS) 
+else
+  $(error "Invalid mode.")
+endif
+
+INC = -I$(PROJECT_HOME)/include
 FLAGS = -Wall -Wfatal-errors -g $(INC)
-CFLAGS += $(FLAGS) -std=c99
-CXXFLAGS += $(FLAGS) -std=c++11 -fno-exceptions -fno-rtti
+CFLAGS ?= -std=c99
+CXXFLAGS ?= -std=c++11 -fno-exceptions -fno-rtti
 ARFLAGS = rcs
 
+LTO ?= 1
 ifeq ($(LTO),1)
-  OPT_FLAGS += -flto
+  EXTRA_FLAGS += -flto
   LDFLAGS += -flto
   ARFLAGS += --plugin $(COMPILER_HOME)/lib/LLVMgold.so
 endif
 
 $(OBJ_DIR)/%.o: %.c
 	@mkdir -p $(OBJ_DIR)
-	$(CC) $(CFLAGS) -o $(OBJ_DIR)/$@ -c $<
+	$(CC) $(FLAGS) $(CFLAGS) $(EXTRA_FLAGS) -o $(OBJ_DIR)/$@ -c $<
 
 $(OBJ_DIR)/%.o: %.cpp
 	@mkdir -p $(OBJ_DIR)
-	$(CXX) $(CXXFLAGS) -o $@ -c $<
+	$(CXX) $(FLAGS) $(CXXFLAGS) $(EXTRA_FLAGS) -o $@ -c $<
+
+%.o: %.cpp
+	$(CXX) $(FLAGS) $(CXXFLAGS) $(EXTRA_FLAGS) -o $@ -c $<
 
 $(LIB_DIR)/lib%.a: $(OBJ)
 	@mkdir -p $(LIB_DIR)
