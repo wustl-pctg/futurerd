557221c7fe1a8c763e32663657525f0441ab2e05
Robert Utterback <robertu13@gmail.com>
2017-03-06 15:11:21 -0600

    Future functions now call appropriate race detection functions. It's not the best way to do it, but we can refactor later.

--- src/future.hpp
+++ src/future.hpp
@@ -1,5 +1,13 @@
 #include <cassert>
 
+/// @todo{Ideally the race detection functions in the future class
+/// will be split out like the other cilk tool functions.}
+#ifdef RACE_DETECT
+#define RD(func) rd_ ## func
+#else
+#define RD(func)
+#endif
+
 // A hack since I don't want to change the compiler.
 // Asynchronously start foo(x, y) with
 // create_future(type, name, foo, x, y)
@@ -8,13 +16,12 @@
 // If we ever do this in parallel we'll need to do something like a
 // detach.
 #define create_future(T,f,func,args...) \
-  cilk::future<T> f; spawn f.finish(func(args))
+  cilk::future<T> f; f.finish(func(args))
 
 // A future that doesn't automatically finish
 // Requires the programmer to use f.finish(...) in the function
 #define create_future2(T,f,func,args...) \
-  cilk::future<T> f; spawn func(args)
-
+  cilk::future<T> f; func(args)
 
 namespace cilk {
 
@@ -26,10 +33,18 @@ private:
   status m_stat;
   T m_value;
 
+  // Race detection stuff
+#ifdef RACE_DETECT
+
+  void rd_create_future() {}
+  void rd_finish_future() {}
+  void rd_get_future() {}
+#endif
+
 public:
 
-  future() : m_stat(status::STARTED) {}
-  void finish(T val) { m_value = val; m_stat = status::DONE; }
+  future() : m_stat(status::STARTED) { RD(create_future(this)); }
+  void finish(T val) { m_value = val; m_stat = status::DONE; RD(finish_future(this)); }
   T get() { assert(m_stat == status::DONE); return m_value; }
 }; // class future
 
