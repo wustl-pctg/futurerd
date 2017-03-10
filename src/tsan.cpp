ba47f2414fd041fc6e5d8af144038f8fb7e355f5
Robert Utterback <robertu13@gmail.com>
2017-03-10 17:09:09 -0600

    Basically not using tsan file anymore. May go back to using it later if driver.cpp gets too large.

--- src/tsan.cpp
+++ src/tsan.cpp
@@ -2,68 +2,17 @@
 #include <cassert>
 
 #include "tsan.hpp"
+#include "futurerd.hpp"
 #include "debug.hpp"
 
 extern "C" {
 
-static bool g_instr_enabled = false;
-__thread int t_checking_disabled = 1;
+//bool check_enable_instrumentation = true;
+// When either is set to false, no errors are output
+//bool instrumentation = false;
+// needs to be reentrant due to reducer operations; 0 means checking
+//bool t_checking_disabled = false;
+bool t_checking_disabled = true;
 
-extern void cilk_tool_init(void);
-extern void cilk_tool_destroy(void);
-
-void __tsan_destroy() {
-  cilk_tool_destroy();
-  g_instr_enabled = false;
-  assert(t_checking_disabled == 0);
-  disable_checking();
-}
-
-void __tsan_init() {
-
-  static bool init = false;
-
-  /// @TODO{For some reason __tsan_init gets called twice...?}
-  // if (init) return;
-  assert(init == false);
-  init = true;
-
-  atexit(__tsan_destroy);
-
-  cilk_tool_init();
-  g_instr_enabled = true; 
-  enable_checking();
-}
-
-[[gnu::always_inline]] static
-bool should_check() { return(g_instr_enabled && t_checking_disabled == 0); }
-
-
-static inline
-void tsan_read(void *addr, size_t mem_size, void *rip) {
-  if (should_check()) DBG_TRACE("checking enabled\n");
-  else DBG_TRACE("checking disabled\n");
-}
-
-static inline
-void tsan_write(void *addr, size_t mem_size, void *rip) {
-  if (should_check()) DBG_TRACE("checking enabled\n");
-  else DBG_TRACE("checking disabled\n");
-}
-
-void __tsan_read1(void *addr) { tsan_read(addr, 1, __builtin_return_address(0)); }
-void __tsan_read2(void *addr) { tsan_read(addr, 2, __builtin_return_address(0)); }
-void __tsan_read4(void *addr) { tsan_read(addr, 4, __builtin_return_address(0)); }
-void __tsan_read8(void *addr) { tsan_read(addr, 8, __builtin_return_address(0)); }
-void __tsan_read16(void *addr) { tsan_read(addr, 16, __builtin_return_address(0)); }
-void __tsan_write1(void *addr) { tsan_write(addr, 1, __builtin_return_address(0)); }
-void __tsan_write2(void *addr) { tsan_write(addr, 2, __builtin_return_address(0)); }
-void __tsan_write4(void *addr) { tsan_write(addr, 4, __builtin_return_address(0)); }
-void __tsan_write8(void *addr) { tsan_write(addr, 8, __builtin_return_address(0)); }
-void __tsan_write16(void *addr) { tsan_write(addr, 16, __builtin_return_address(0)); }
-void __tsan_func_entry(void *pc){ }
-void __tsan_func_exit() { }
-void __tsan_vptr_read(void **vptr_p) {}
-void __tsan_vptr_update(void **vptr_p, void *new_val) {}
 
 } // extern "C"
--- src/tsan.hpp
+++ src/tsan.hpp
@@ -2,20 +2,11 @@
 
 extern "C" {
 
-extern __thread int t_checking_disabled;
-
-#define INLINE __attribute__((always_inline))
-//[[gnu::always_inline]]
-
-inline extern void enable_checking() {
-  t_checking_disabled--;
-  assert(t_checking_disabled >= 0);
-}
-
-
-inline extern void disable_checking() {
-  assert(t_checking_disabled >= 0);
-  t_checking_disabled++;
-}
-
 } // extern "C"
+
+namespace tsan {
+
+
+} // namespace tsan
+
+//} // extern "C"
