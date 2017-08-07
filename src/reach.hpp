0b95aa5a401f6a6e82e96a6e2f9e7d79b44838d1
Robert Utterback <robertu13@gmail.com>
2017-08-07 10:56:59 -0500

    Added ability to generate ids for the general reachability data structure

--- src/reach.hpp
+++ src/reach.hpp
@@ -7,12 +7,14 @@ class reachability {
 public:
   using node = std::size_t;
   void add_edge(node from, node to);
+  node add_node();
   bool precedes(node x, node y) const;
   
 private:
-
+  static size_t global_index = 1; // 0 is fake, for representing nodes
+                                  // not in R
+  
   /// @todo{Use of std::vector for the reachability matrix will
   /// probably need to be rewritten for performance.}
   std::vector< std::vector<bool> > data;
-
 }; // class reachability
