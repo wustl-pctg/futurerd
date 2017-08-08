192c6f5615b2b019bfb09dacb5a0e49a6ebb14b5
Robert Utterback <robertu13@gmail.com>
2017-08-08 17:14:35 -0500

    Updating main futurerd file from structured branch

--- src/futurerd.cpp
+++ src/futurerd.cpp
@@ -1,6 +1,4 @@
 #include "futurerd.hpp"
-#include "utils/union_find.hpp"
-#include "sp_reach.hpp"
 #include "shadow_stack.hpp"
 #include "debug.hpp"
 
@@ -12,44 +10,64 @@
 
 namespace futurerd {
 
-typedef utils::uf::node set_t;
-
-struct frame_data { sp_node curr, sync; };
+// Actually, I think I may only need the Pbag, and just to keep a
+// thread-local "active" Sbag...
+struct frame_data {
+  spbag *Sbag, *Pbag;
+  //  spbag *active; // do we need this? Would it just be head()->Sbag?
+};
 
 struct access_info {
-  sp_node last_reader;
-  sp_node last_writer;
+  spbag *last_reader;
+  spbag *last_writer;
 };
 
 // For parallel detection we will need this to be thread
 // local. Actually, it will need to be a P-sized array since we need
 // to access other workers' shadow stacks.
-shadow_stack<frame_data> t_sstack;
-sp_reachability g_sp;
+/*__thread*/ shadow_stack<frame_data> t_sstack;
+shadow_mem g_shadow_mem;
+
+/*static*/ /*__thread*/ bool t_clear_stack = false;
 
 /// XXX: Should be detector state object
 static DetectPolicy g_policy = futurerd::DetectPolicy::ABORT;
 static size_t g_num_races = 0;
 
-// temporary variables for testing
-void* g_loc;
-access_info g_access = {};
+// originally this was set to false, but why?
+static /*__thread*/ bool t_checking_disabled = true;
 
 void set_policy(DetectPolicy p) {
-  if (p >= DetectPolicy::END_POLICY) debug::die("Invalid Detection Policy\n");
+  if (p >= DetectPolicy::END_POLICY)
+    debug::die("Invalid Detection Policy\n");
   g_policy = p;
 }
 
 size_t num_races() { return g_num_races; }
 
-void set_loc(void* addr) { g_loc = addr; }
-
-sp_node* active_sp() {
-  frame_data *f = t_sstack.head();
-  assert(f && f->curr.english);
-  return &f->curr;
+//__attribute__((always_inline))
+void enable_checking() {
+  //assert(t_checking_disabled);
+  t_checking_disabled = false;
 }
 
+//__attribute__((always_inline))
+void disable_checking() {
+  //assert(!t_checking_disabled);
+  t_checking_disabled = true;
+}
+
+//[[gnu::always_inline]] static
+bool should_check() { return !t_checking_disabled; }
+//{ return(g_instr_enabled && t_checking_disabled == 0); }
+
+inline spbag* active() {
+  spbag *f = t_sstack.head()->data.Sbag;
+  assert(f);
+  return f;
+}
+
+// Later on, report what kind of race, i.e. RW, WR, or WW
 void report_race(void* addr) {
   if (! (g_policy == futurerd::DetectPolicy::SILENT) )
     fprintf(stderr, "Race detected at %p\n", addr);
@@ -58,45 +76,45 @@ void report_race(void* addr) {
     std::abort();
 }
 
-void check_read(void* addr) {
-  if (addr != g_loc) return;
+void check_access(bool is_read, void* rip, void* addr, size_t mem_size) {
+  shadow_mem::addr_info_t *slot = g_shadow_mem.find((uint64_t)addr);
 
-  sp_node *curr = active_sp();
-  sp_node *last_writer = &g_access.last_writer;
-  if (last_writer->english) { // has a writer
-    if (g_sp.logically_parallel(last_writer, curr))
-      report_race(addr);
+  // no previous accesses
+  if (slot == nullptr) {
+    g_shadow_mem.insert(is_read, (uint64_t)addr, active(), (uint64_t)rip);
+    return;
   }
-  g_access.last_reader = *curr;
+  
+  // check race with last writer
+  if (slot->last_writer.access != nullptr // if last writer exists
+      && !slot->last_writer.access->precedes_now())
+    report_race(addr);
+  
+  // if write, check race with last read
+  if (!is_read // if a write
+      && slot->last_reader.access != nullptr // and last reader exists
+      && !slot->last_reader.access->precedes_now()) {
+    report_race(addr);
+  }
+  
+  // update shadow mem
+  g_shadow_mem.update(slot, is_read, (uint64_t)addr, active(), (uint64_t)rip);
 }
 
-void check_write(void* addr) {
-  if (addr != g_loc) return;
-  sp_node *curr = active_sp();
-  
-  sp_node *last_writer = &g_access.last_writer;
-  if (last_writer->english) { // has a writer
-    if (g_sp.logically_parallel(last_writer, curr))
-      report_race(addr);
-  }
-  
-  sp_node *last_reader = &g_access.last_reader;
-  if (last_reader->english) { // has a last reader
-    if (g_sp.logically_parallel(last_reader, curr))
-      report_race(addr);
-  }
-
-  g_access.last_writer = *curr;
+// either initialization, spawn or future creation
+void new_function() {
+  frame_data *f = &t_sstack.head()->data;
+  f->Sbag = new spbag(spbag::bag_kind::S);
+  f->Pbag = nullptr; // will create this lazily when we need it
+  //f->active = f->Sbag;
 }
 
 
 void init() {
   //___cilkrts_set_nworkers(1);
   __cilkrts_set_param("nworkers", "1");
-  t_sstack.push();
-  frame_data *f = t_sstack.head();
-  f->curr = g_sp.first();
-  f->sync = {0};
+  // t_sstack.push();
+  // new_function();
 }
 
 void destroy() {
@@ -106,64 +124,147 @@ void destroy() {
   //   shadow stack
 }
 
-void reset() { destroy(); init(); }
+// This should do things like settings stack_low_watermark. Things
+// that need to be reset whenever we re-enter Cilk after a break.
+void reset() { }
 void print_stats(FILE *output = stdout) { }
 
+/// TODO: some duplication between this and at_spawn
 void at_create(future_rd_info *info) {
   // very similar to spawn
+  t_sstack.push_future();
+  new_function();
 }
 
-void at_finish(future_rd_info *info) { }
+// Returning from a spawned function or future function
+void at_continuation() {
+  frame_data *f = &t_sstack.head()->data;
 
-void at_get(future_rd_info *info) { }
+  // XXX: can't we just change f->Sbag to be a bag, then assign it to p->Pbag?
+  frame_data *p = &t_sstack.parent()->data;
+
+  // if (!p->Pbag) p->Pbag = new spbag(spbag::bag_kind::P);
+  // else p->Pbag->set_kind(spbag::bag_kind::P);
+  // p->Pbag->merge(f->Sbag);
+
+  if (!p->Pbag) 
+    p->Pbag = f->Sbag;
+  else
+    p->Pbag->merge(f->Sbag);
+  // sometimes this bag may have been merged with an S-bag, so it
+  // think it's an Sbag. But now it must be a Pbag. See basic/test6.
+  // XXX: is there a cleaner way to do this?
+  spbag::find(p->Pbag)->set_kind(spbag::bag_kind::P);
+  //p->Pbag->set_kind(spbag::bag_kind::P);
+
+  // XXX: can't we just change f->Sbag to be a Pbag, rather than
+  // creating a new bag?
+  // if (!f->Pbag) f->Pbag = new spbag(spbag::bag_kind::P);
+  // f->Pbag->merge(f->Sbag);
+}
 
 void at_put(future_rd_info *info) {
-
-  sp_node *curr = active_sp();
-
-  sp_node tmp;
-  g_sp.insert_sequential(curr, &tmp);
-  *curr = tmp;
+  assert(0); // implement me!
 }
 
-void at_cilk_function() {
+// similar to at_leave_begin
+void at_finish(future_rd_info *info) {
+  at_continuation();
+  assert(t_sstack.in_future_helper());
+
+  // when using fake futures, no cilk_leave_frame is called.
+  t_clear_stack = true;
+
+  //info->put_strand = t_sstack.head()->data.Pbag;
+  t_sstack.pop();
+  info->put_strand = t_sstack.head()->data.Pbag;
+
+
+}
+
+void at_get(future_rd_info *info) {
+  assert(info->put_strand);
+  t_sstack.head()->data.Sbag->merge(info->put_strand);
+}
+
+// A sync is equivalent to calling "get" on all the futures that were
+// "spawned" in this block
+void at_sync() {
+  frame_data *f = &t_sstack.head()->data;
+
+  // make sure it's a real sync
+  if (!f->Pbag) return; 
+
+  f->Sbag->merge(f->Pbag);
+  f->Pbag = nullptr;
+
+}
+
+bool at_cilk_function() {
   t_sstack.push_spawner();
-  frame_data *f = t_sstack.head();
-  frame_data *p = t_sstack.ancestor(1);
-  *f = *p;
-  assert(f->curr.english);
-  assert(f->curr.hebrew);
+  frame_data *f = &t_sstack.head()->data;
+  if (__builtin_expect(t_sstack.size() > 1, true)) {
+
+    // I *think* instead I should just keep a thread-local "active"
+    // strand...
+    frame_data *p = &t_sstack.parent()->data;
+    //*f = *p;
+    f->Sbag = p->Sbag;
+    
+    return false;
+  } else {
+    f->Sbag = new spbag(spbag::bag_kind::S);
+    f->Pbag = nullptr; // will create this lazily when we need it
+    return true;
+  }
 }
 
 // f is the new frame
 void at_spawn() {
   t_sstack.push_helper();
-  frame_data *f = t_sstack.head();
-  frame_data *p = t_sstack.ancestor(1);
-
-  if (!p->sync.english) // first of spawn group, set sync node
-    g_sp.insert_sequential(&p->curr, &p->sync);
-
-
-  sp_node tmp;
-  g_sp.fork(&p->curr, &f->curr, &tmp);
-  p->curr = tmp;
-  f->sync = {0};
+  new_function();
 }
 
-void at_leave_begin() { t_sstack.pop(); }
+void at_leave_begin() {
+  if (t_sstack.in_spawn_helper()) {
 
-bool at_leave_end() { return t_sstack.size() == 1; }
+    at_continuation();
 
-void at_continuation() { t_sstack.pop(); }
+    // This is the point where we need to set flag to clear accesses to stack 
+    // out of the shadow memory.  The spawn helper of this leave_begin
+    // is about to return, and we want to clear stack accesses below 
+    // (and including) spawn helper's stack frame.  Set the flag here
+    // and the stack will be cleared in tsan_func_exit.
+    t_clear_stack = true;
+    
+  } else if (t_sstack.size() > 1) {
 
-void at_sync() {
-  frame_data *f = t_sstack.head();
+    // we may have strands saved in the parent's Pbag. In other words,
+    // we may be unsynced.
+    frame_data *f = &t_sstack.head()->data;
+    frame_data *p = &t_sstack.parent()->data;
 
-  if (f->sync.english) { // make sure it's a real sync
-    f->curr = f->sync;
-    f->sync = {0};
+    assert(f->Sbag);
+    p->Sbag = f->Sbag;
+
+    if (f->Pbag) {
+      assert(f->Pbag->kind == spbag::bag_kind::P);
+      if (!p->Pbag) {
+        p->Pbag = f->Pbag;
+      } else {
+        assert(p->Pbag->kind == spbag::bag_kind::P);
+        p->Pbag->merge(f->Pbag);
+      }
+    }
+    //t_sstack.parent()->data = t_sstack.head()->data;  
+    // uf::merge(p->S, f->S);
   }
+  // Copy to parent frame
+  
+  t_sstack.pop();
 }
+
+// Last frame was already popped in at_leave_begin()
+bool at_leave_end() { return t_sstack.size() == 0; }
 
 } // namespace futurerd
--- src/futurerd.hpp
+++ src/futurerd.hpp
@@ -1,3 +1,6 @@
+#include "spbag.hpp"
+#include "shadow_mem.hpp"
+
 #include <cstdlib> // size_t
 
 namespace futurerd {
@@ -10,18 +13,20 @@ enum DetectPolicy {
 };
 
 struct future_rd_info {
-
+  spbag* put_strand;
 }; // struct future_rd_info
+
+extern shadow_mem g_shadow_mem;
+extern /*__thread*/ bool t_clear_stack;
 
 // Public API
 void set_policy(futurerd::DetectPolicy p);
 size_t num_races();
+void enable_checking();
+void disable_checking();
+bool should_check();
 
-// Fake, just for testing. Sets the memory location we want to check
-// for races
-void set_loc(void* addr);
-void check_read(void *addr);
-void check_write(void *addr);
+void check_access(bool is_read, void* rip, void* addr, size_t mem_size);
 
 // Tool API (for cilktool and tsan)
 void reset();
@@ -29,11 +34,13 @@ void init();
 void destroy();
 
 void at_create(future_rd_info*);
+void at_put(future_rd_info*);
 void at_finish(future_rd_info*);
 void at_get(future_rd_info*);
-void at_put(future_rd_info*);
+// at_start(future_rd_info*); // not yet used
 
-void at_cilk_function();
+// returns whether or not we're at the first frame
+bool at_cilk_function();
 void at_leave_begin();
 
 // Returns whether or not we're leaving the last frame
