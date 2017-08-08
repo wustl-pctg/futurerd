3e84e4a4fee9fa88ef91455467bae38bd3a03dc7
Robert Utterback <robertu13@gmail.com>
2017-08-08 17:12:20 -0500

    Adding shadow mem files

---
+++ src/shadow_mem.cpp
@@ -0,0 +1,42 @@
+#include "shadow_mem.hpp"
+#include <cassert>
+
+using addr_t = shadow_mem::addr_t;
+
+shadow_mem::addr_info_t* shadow_mem::find(addr_t key) {
+  auto it = m_shadow_map.find(key);
+  return (it != m_shadow_map.end()) ? &it->second : nullptr;
+}
+
+shadow_mem::addr_info_t* shadow_mem::insert(bool is_read, addr_t addr,
+                                            rd_info_t strand, addr_t rip) {
+  addr_info_t *old = find(addr);
+  addr_info_t *slot = old;
+  if (slot == nullptr) {
+    auto option = m_shadow_map.insert(std::make_pair(addr, addr_info_t()));
+    assert(option.second); // indicates success of insert operation
+    slot = &option.first->second;
+  }
+  assert(slot);
+  access_t *access = (is_read) ? &slot->last_reader : &slot->last_writer;
+  *access = access_t(strand, rip);
+  return old;
+}
+
+void shadow_mem::update(addr_info_t *slot, bool is_read,
+            addr_t addr, rd_info_t strand, addr_t rip) {
+  assert(slot != nullptr);
+  access_t *access = (is_read) ? &slot->last_reader : &slot->last_writer;
+  *access = access_t(strand, rip);
+}
+
+void shadow_mem::clear(addr_t start, addr_t end) {
+  //assert(ALIGN_BY_NEXT_MAX_GRAIN_SIZE(end) == end); 
+  assert(start < end);
+  assert(end-start < 4096);
+
+  while(start != end) {
+    m_shadow_map.erase(start);
+    start += GRAIN_SIZE;
+  }
+}
---
+++ src/shadow_mem.hpp
@@ -0,0 +1,63 @@
+#pragma once
+#include <cstdio>
+#include <map>
+
+#include "spbag.hpp"
+
+// Some of the driver code uses this stuff...
+#define LOG_KEY_SIZE  4
+#define LOG_TBL_SIZE 20
+
+#define GRAIN_SIZE 4
+#define LOG_GRAIN_SIZE 2
+#define MAX_GRAIN_SIZE (1 << LOG_KEY_SIZE)
+#define NUM_SLOTS (MAX_GRAIN_SIZE / GRAIN_SIZE)
+ 
+// a mask that keeps all the bits set except for the least significant bits
+// that represent the max grain size
+#define MAX_GRAIN_MASK (~(uint64_t)(MAX_GRAIN_SIZE-1))
+
+// If the value is already divisible by MAX_GRAIN_SIZE, return the value; 
+// otherwise return the previous / next value divisible by MAX_GRAIN_SIZE.
+#define ALIGN_BY_PREV_MAX_GRAIN_SIZE(addr) ((uint64_t) (addr & MAX_GRAIN_MASK))
+#define ALIGN_BY_NEXT_MAX_GRAIN_SIZE(addr)                  \
+  ((uint64_t) ((addr+(MAX_GRAIN_SIZE-1)) & MAX_GRAIN_MASK))
+
+// compute (addr % 16) / GRAIN_SIZE 
+#define ADDR_TO_MEM_INDEX(addr)                                       \
+  (((uint64_t)addr & (uint64_t)(MAX_GRAIN_SIZE-1)) >> LOG_GRAIN_SIZE)
+
+// compute size / GRAIN_SIZE 
+#define SIZE_TO_NUM_GRAINS(size) (size >> LOG_GRAIN_SIZE) 
+
+
+//template<class mem_access_data>
+class shadow_mem {
+public:
+  using addr_t = uint64_t;
+  using rd_info_t = spbag*;
+  struct access_t {
+    rd_info_t access;
+    addr_t rip;
+    access_t() : access(nullptr), rip(0) {}
+    access_t(rd_info_t a, addr_t r)
+      : access(a), rip(r) {}
+  };
+
+  struct addr_info_t {
+    access_t last_reader;
+    access_t last_writer;
+    addr_info_t() {}
+  };
+
+  addr_info_t* find(addr_t addr);
+
+  // return the old value
+  addr_info_t* insert(bool is_read, addr_t addr, rd_info_t strand, addr_t rip);
+  void update(addr_info_t *slot, bool is_read,
+              addr_t addr, rd_info_t strand, addr_t rip);
+  void clear(addr_t start, addr_t end);
+
+private:
+    std::map<addr_t, addr_info_t> m_shadow_map;
+};
