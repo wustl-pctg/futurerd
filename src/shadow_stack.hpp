8257afdbe685d3668f7507b6fbdde6a47f347562
Robert Utterback <robertu13@gmail.com>
2017-10-05 09:52:01 -0500

    Various old shadow stack updates...probably temporary

--- src/shadow_stack.hpp
+++ src/shadow_stack.hpp
@@ -4,6 +4,12 @@
 
 template <typename T>
 struct shadow_frame {
+  enum frame_type : unsigned char { SPAWNER = 0, HELPER};
+  frame_type type;
+  
+  bool synced = true; // is this frame "synced"? Need this because of the
+               // implicit sync at the end of every Cilk function
+  
   T data;
 }; // struct shadow_frame
 
@@ -15,11 +21,46 @@ class shadow_stack : public utils::stack< shadow_frame<T> > {
 
 public:
 
-  T* push() { base_type::push(); return this->head(); }
+  T* push() {
+    frame* f = base_type::push();
+    f->synced = true;
+    return this->head();
+  }
   T* head() const { return ancestor(0); }
   T* parent() const { return ancestor(1); }
   T* ancestor(int i) const {
     frame *f = base_type::ancestor(i);
     return &f->data;
+  }
+
+  T* push_spawner() {
+    frame* f = base_type::push();
+    f->synced = true;
+    f->type = frame::frame_type::SPAWNER;
+    return this->head();
+  }
+
+  T* push_helper() {
+    frame* f = base_type::push();
+    f->synced = true;
+    f->type = frame::frame_type::HELPER;
+    return this->head();
+  }
+  
+  // XXX: ???
+   T* spawn() {
+     // Current frame is no longer synced, if it was
+     base_type::head()->synced = false;
+     frame* f = base_type::push();
+     f->synced = true;
+     f->type = frame::frame_type::SPAWNER;
+     return this->head();
+   }
+  // Returns whether or not we were already synced
+  bool sync() {
+    frame* f = base_type::head();
+    bool old = f->synced;
+    f->synced = true;
+    return old;
   }
 }; // class shadow_stack
