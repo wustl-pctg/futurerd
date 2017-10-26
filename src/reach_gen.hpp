b94b8edf07d850f2fa727304dc3a095120134ec3
Robert Utterback <robertu13@gmail.com>
2017-10-26 11:48:13 -0500

    Allowed easy switching between reachability data structures using Makefile

--- src/reach_gen.hpp
+++ src/reach_gen.hpp
@@ -1,4 +1,4 @@
-// General reachability data sructure
+// General reachability data structure
 
 #include <cstddef>
 #include <vector>
@@ -6,15 +6,14 @@
 namespace reach {
 class general {
 public:
-  //struct sframe_data {};
+
   using node = std::size_t;
   void add_edge(node from, node to);
   node add_node();
   bool precedes(node x, node y) const;
   
 private:
-  static size_t global_index = 1; // 0 is fake, for representing nodes
-                                  // not in R
+  static size_t global_index;
   
   /// @todo{Use of std::vector for the reachability matrix will
   /// probably need to be rewritten for performance.}
