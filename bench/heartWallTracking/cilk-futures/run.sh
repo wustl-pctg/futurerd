21993e18d4ee2f7be721bffeaba6d33b88de868f
Angelina Lee <angelee@gmail.com>
2017-10-31 18:10:39 -0500

    Moving files around, update Makefile

--- bench/heartWallTracking/cilk-futures/run.sh
+++ bench/heartWallTracking/cilk-futures/run.sh
@@ -2,4 +2,5 @@
 # The first param is the number of frames to process, 
 # second is num of omp threads
 # original input: 20 4
-time ./heartwall ../data/test.avi 104 32 
+# full input: 104 1
+time ./heartwall ../data/test.avi 10 1
--- bench/heartWallTracking/openmp/run.sh
+++ bench/heartWallTracking/openmp/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # The first param is the number of frames to process, 
 # second is num of omp threads
-# original input: 20 4
-time ./heartwall ../data/test.avi 104 1
+# original input: 104 32 
+time ./heartwall ../data/test.avi 10 1 
