18a1601d67c0a8b792b086f4f63964cb4ca18735
Robert Utterback <robertu13@gmail.com>
2017-10-31 11:36:38 -0500

    nonblocking updates

--- src/reach_nonblock.cpp
+++ src/reach_nonblock.cpp
@@ -1,158 +1,226 @@
 #include "reach_nonblock.hpp"
-
-// XXX: Make initial node, which must be attached.
-
-using namespace spbags;
+#include <cassert>
 
 namespace reach {
 
-/********** Parallelism Creation **********/
-// Outgoing non-SP edge
-void nonblock::at_future_create(sframe_data *f) {
-  // Basically spawn without worrying about sync nodes
-  structured::at_future_create(&f->sp);
+using node = nonblock::node;
 
-  node *u = t_current;
-  u->attach();
+nonblock::nonblock(sframe_data *initial) { init(initial); }
 
-  node *w = new node(); // left (executed next)
-  node *v = new node(); // right
-
-  w->id = m_R.add_node();
-  m_R.add_edge(find(u)->id, w->id);
-  v->id = m_R.add_node();
-  m_R.add_edge(find(u)->id, w->id);
-
-  f->last_cont = v;
-  t_current = w;
+nonblock::smem_data* nonblock::active(sframe_data *f) {
+  return t_current;
 }
 
-void nonblock::at_spawn(sframe_data *f) {
-  structured::at_spawn(&f->sp);
+// Should be done in a constructor if possible
+void nonblock::init(sframe_data *initial) {
+  m_struct.init(&initial->sp);
+  t_current = new node();
+  t_current->att_pred = t_current;
+  attachify(t_current);
+}
+
+void nonblock::attachify(node* n) {
+  node *su = n->find();
+  if (su->attached()) return;
+
+  // su is unattached
+  su->id = m_R.add_node(); // make attached with node in R
+  assert(su->att_pred);
+  m_R.add_edge(su->att_pred->find()->id, su->id);
+}
+
+/********** Parallelism Creation **********/
+
+void nonblock::begin_strand(sframe_data *f, sframe_data *p) {
+  m_struct.begin_strand(&f->sp,&p->sp);
+  // Future stuff?
+}
+
+void nonblock::create_strand(sframe_data *f) {
+  m_struct.create_strand(&f->sp);
+}
+
+void nonblock::at_spawn(sframe_data *f, sframe_data *h) {
+  m_struct.at_spawn(&f->sp, &h->sp);
+
+  node *u = t_current; // fork node
+  h->fork = u;
+  h->rfc = h->ljp = nullptr;
+
+  // Make new unattached set containing v
+  node *v = new node(); // left child
+  h->lfc = v;
+  v->att_pred = (u->find()->attached()) ? u : u->find()->att_pred;
+
+  //h->nodes.push_fork(u, w, v);
+  // v->att_pred = u->find()->att_pred;
+  // w->att_pred = u->find()->att_pred;
+
+  // XXX anything else for the other child? Set att_pred?
+  // node *w = new node(); // right child
+  // h->rfc = w;
+  // h->cont = w;
+
+  t_current = v;
+}
+
+// Outgoing non-SP edge
+// We don't have helper frames for futures, so this is actually the
+// new frame created for the new strand
+void nonblock::at_future_create(sframe_data *f) {
+  // Basically spawn without worrying about sync nodes
+  m_struct.at_future_create(&f->sp);
 
   node *u = t_current;
-  node *w = new node(); // left
-  node *v = new node(); // right
+  assert(f->future_fork == nullptr);
+  f->future_fork = u;
+  attachify(u);
 
-  f->nodes.push_fork(u, w, v);
+  // Create a new attached set
+  node *v = new node(); // left (executed next, since we use eager execution)
+  v->id = m_R.add_node();
+  m_R.add_edge(u->find()->id, v->id);
 
-  // XXX: What if u is already attached??
-  // Wouldn't we need something like this:
-  // t_current->att_pred = (find(f)->attached()) ? f : find(f)->att_pred;
-  v->add_pred = find(u)->att_pred;
-  w->add_pred = find(u)->att_pred;
+  //node *w = new node(); // right
+  // w->id = m_R.add_node();
+  // m_R.add_edge(u->find()->id, w->id);
 
-  f->last_cont = v;
-  t_current = w;
+  //f->cont = w;
+  t_current = v;
 }
 
 /********** Parallelism Deletion **********/
 // This won't be called until the future has actually finished
+// Incoming non-SP edge
 void nonblock::at_future_get(sframe_data *f, sfut_data *fut) {
   assert(fut->put_strand);
 
   node *u = t_current;
-  node *v = new node(m_R.add_node()); // make attached
+
+  // Make a new attached set
+  node *v = new node(m_R.add_node());
+  m_R.add_edge(u->find()->id, v->id);
+
+  // XXX: Not in the pseudocode but I don't see how you couldn't have this.
   node *w = fut->put_strand;
-
-  m_R.add_edge(find(u)->id, v->id);
-
-  assert(find(w)->attached());
-  m_R.add_edge(find(w)->id, v->id);
+  assert(w->find()->attached());
+  m_R.add_edge(w->find()->id, v->id);
 
   t_current = v;
 }
 
 // Returns the new node j
 node* nonblock::binary_join(node *f, // fork node
-                  node *lfc, node *rfc, // left, right fork children
-                  node *ljp, node *rjp // left, right join children
-                  ) {
+                            node *lfc, node *rfc, // left, right fork children
+                            node *ljp, node *rjp // left, right join children
+                            ) {
   node *j = new node(); // join node
+  bool ljp_attached = ljp->find()->attached();
+  bool rjp_attached = rjp->find()->attached();
 
   // No non-SP edges
-  if (!find(p)->attached() && !find(p)->attached()) {
+  if (!ljp_attached && !rjp_attached) {
     f->merge(ljp);
     f->merge(rjp);
     f->merge(j);
 
-    // Both ides incident on non-SP edges
-  } else if (find(p)->attached() && find(p)->attached()) {
-    f->attach();
-    m_R.add_edge(find(f)->id, find(c)->id);
-    m_R.add_edge(find(f)->id, find(c)->id);
+    // Both sides incident on non-SP edges
+  } else if (ljp_attached && rjp_attached) {
+    //f->attach();
+    attachify(f);
+    m_R.add_edge(f->find()->id, lfc->find()->id);
+    m_R.add_edge(f->find()->id, rfc->find()->id);
 
     j->id = m_R.add_node();
-    m_R.add_edge(find(p)->id, j->id);
-    m_R.add_edge(find(p)->id, j->id);
+    m_R.add_edge(ljp->find()->id, j->id);
+    m_R.add_edge(rjp->find()->id, j->id);
 
   } else {
     node *att_pred, *unatt_pred; // attached/unattached join parents
     node *att_succ, *unatt_succ; // attached/unattached fork children
-    if (find(p)->attached()) {
-      att_pred = find(p); att_succ = find(c);
-      unatt_pred = find(p); unatt_pred = find(c);
+    if (ljp_attached) {
+      att_pred = ljp->find(); att_succ = lfc->find();
+      unatt_pred = rjp->find(); unatt_succ = rfc->find();
     } else {
-      att_pred = find(p); att_succ = find(c);
-      unatt_pred = find(p); unatt_pred = find(c);
+      att_pred = rjp->find(); att_succ = rfc->find();
+      unatt_pred = ljp->find(); unatt_succ = lfc->find();
     }
     assert(att_pred->attached() && att_succ->attached());
     assert(!unatt_pred->attached() && !unatt_succ->attached());
 
-    if (!find(f)->attached()) f->merge(att_succ);
+    if (!f->find()->attached()) f->merge(att_succ);
     att_pred->merge(j);
-    unatt_find(d)->att_succ = find(j);
+    unatt_pred->find()->att_succ = j->find();
   }
 
   return j;
 }
 
-void nonblock::at_sync(sframe_data *frame) {
-  
-  node *f; // fork 
-  node *s1; // left fork child (s_1)
-  node *s2; // right fork child (s_2)
-  node *t1; // left join parent (t_1)
-  node *t2 = t_current; // right join parent (t_2)
+// Currently we only support binary join
+void nonblock::at_sync(sframe_data *f) {
 
-  
-  while (frame->nodes.pop(&f, &s1, &s2, &t1)) {
-    t2 = binary_join(f, s1, s2, t1, t2);
-  }
+  //node *f; // fork
+  // node *s1; // left fork child (s_1)
+  // node *s2; // right fork child (s_2)
+  // node *t1; // left join parent (t_1)
+  //node *t2 = t_current; // right join parent (t_2)
+
+
+  // while (frame->nodes.pop(&f, &s1, &s2, &t1)) {
+  //   t2 = binary_join(f, s1, s2, t1, t2);
+  // }
 
   // Set current node
-  t_current = t2;
+  //t_current = t2;
+
+  assert(f->fork); assert(f->lfc); assert(f->rfc); assert(f->ljp);
+  t_current = binary_join(f->fork, f->lfc, f->rfc, f->ljp, t_current);
+  f->fork = f->lfc = f->rfc = f->ljp = nullptr;
 }
 
 /********** Continuations **********/
 // Returning from a spawned function or future function.
 // This is all the other non-join nodes, right?
-void nonblock::continuation(sframe_data *f) {
-  t_current = f->cont;
-  f->cont = nullptr;
-}
+void nonblock::continuation(sframe_data *f) { t_current = new node(); }
 
+// f is the helper frame, p is parent
 void nonblock::at_spawn_continuation(sframe_data *f, sframe_data *p) {
-    structured::at_continuation(&f->sp);
-    continuation(f);
+  // Pop hasn't happened yet
+  assert(f->fork); assert(f->lfc); assert(f->rfc == nullptr);
+  m_struct.at_spawn_continuation(&f->sp, &p->sp);
+
+  f->ljp = t_current;
+  continuation(f);
+  f->rfc = t_current;
+  *p = *f;
 }
 
 void nonblock::at_future_continuation(sframe_data *f, sframe_data *p) {
-  continuation(f);
+  // Use p because the pop hasn't happened yet
+  // Except we're not really doing anything with the "future helper"
+  // frames...do we even need them?
+  //continuation(p);
+  assert(f->future_fork);
+  node *w = new node(); // right
+  w->id = m_R.add_node();
+  m_R.add_edge(f->future_fork->find()->id, w->id);
+  f->future_fork = nullptr;
+  t_current = w;
 }
 
 // Called when a future task completes. We know we're going to have
 // non-SP edges out from here, we just don't know where to yet.
-void nonblock::at_future_finish(sfut_data *fut) {
+void nonblock::at_future_finish(sframe_data *f, sframe_data *p, sfut_data* fut) {
   // Save for later, i.e. at_get
   fut->put_strand = t_current;
 
   // NB: Is it okay to do this here? Seems easier than during
   // at_get(), since a future may have get() called on it multiple
   // times.
-  // t_current->attach();
+  attachify(t_current);
+
+  // The only way we know of a future continuation, currently.
+  at_future_continuation(f, p);
 }
-
-
 
 } // namespace reach
--- src/reach_nonblock.hpp
+++ src/reach_nonblock.hpp
@@ -3,14 +3,7 @@
 // We don't actually need the structured part, just the fork/join part
 // of SP Bags.
 #include "reach_structured.hpp"
-
-// XXX: move this
-// This is just a template that the real reachability data structures
-// must follow. You could probably mess around with virtual functions
-// and the like (although I don't know about requiring the definition
-// of an sframe_data struct). But (1) I'm not sure that's any clearer,
-// and (2) the virtual functions may add overhead if the program uses
-// enough parallel operations.
+#include "reach_gen.hpp"
 
 namespace reach {
 class nonblock {
@@ -19,71 +12,35 @@ public:
 
     node *att_pred = nullptr;
     node *att_succ = nullptr;
-    genreach::node id; // id of node in R graph, or 0 if not in R (size_t)
+    reach::general::node id; // id of node in R graph, or 0 if not in R (size_t)
     bool attached() { return id > 0; }
-
-    node(genreach::node _id = 0) : id(_id) {}
-  
-    // make the set containing u attached if not already ("attachify")
-    void attach() {
-      node *su = this->find();
-      if (su->attached()) return;
-    
-      // su is unattached
-      su->id = g_R.add_node(); // make attached with node in R
-      g_R.add_edge(find(su->att_pred)->id, su->id);
-    }
+    node* find() { return static_cast<node*>(utils::uf::find(this)); }
+    bool precedes_now() { return true; }
+    node(reach::general::node _id = 0) : id(_id) {}
+    void merge(node *n) { utils::uf::merge(this, n); }
   }; // struct node
-  
-  // Helper data structure for shadow frames
-  struct node_stack {
-    using index_t = uint8_t;
-    static constexpr index_t STACK_SIZE = 4;
-    index_t index = 0;
-    node* fork_nodes[STACK_SIZE];
-    node* cont_nodes[STACK_SIZE]; // do we need this? Duplication with last_cont
-    node* succ_nodes[STACK_SIZE];
-    node* left_pred_nodes[STACK_SIZE];
-
-    void push_fork(node *fork, node* succ, node* cont) {
-      assert(index < STACK_SIZE);
-      fork_nodes[index] = fork;
-      succ_nodes[index] = succ;
-      cont_nodes[index] = cont;
-
-      // "finalize" the fork
-      void push_succ(node *left_pred) { left_pred_nodes[index++] = left_pred; }
-      node* get_last_cont() { return cont_nodes[index - 1]; }
-  
-      // returns remaining size
-      index_t pop(node** fork, node** succ, node** cont, node** left_pred) {
-        if (index == 0) return 0;
-        index--;
-        *fork = fork_nodes[index];
-        *cont = cont_nodes[index];
-        *succ = succ_nodes[index];
-        *left_pred = left_pred_nodes[index];
-        return index;
-      }
-    }
-  }; // struct node_stack
   
   struct sframe_data {
     structured::sframe_data sp;
 
     // Future RD stuff.
+    node *fork = nullptr;
+    node *lfc = nullptr;
+    node *rfc = nullptr;
+    node *ljp = nullptr; // rjp is t_current at sync
+
+    node *future_fork = nullptr;
+    
     // continuation node, created and saved at spawns and create_futures
-    node *last_cont; // XXX: Update to use of node_stack?
+    //node *cont = nullptr; // XXX: Update to use of node_stack?
 
     // NB: We will probably need to save some more things here to handle
     // joins with in-degree > 2.
-    node_stack nodes;
+    //node_stack nodes;
   };
 
-  struct smem_data {
-    //spbag *last_reader;
-    //spbag *last_writer;
-  };
+  //struct smem_data {};
+  using smem_data = node;
 
   struct sfut_data {
     node* put_strand;
@@ -91,13 +48,20 @@ public:
     
   // Member data
 private:
-  genreach m_R;
+  reach::general m_R;
+  reach::structured m_struct;
   node* t_current; // XXX: necessary?
 
 public:
+  nonblock() {}
+  nonblock(sframe_data *initial);
+  void init(sframe_data *initial);
+
+  smem_data* active(sframe_data *f);
+  
   // Parallelism creation
   void at_future_create(sframe_data *f);
-  void at_spawn(sframe_data *f);
+  void at_spawn(sframe_data *f, sframe_data *helper);
 
   // Parallelism deletion
   // void at_future_put(sfut_data*);
@@ -106,14 +70,22 @@ public:
 
   // Continuations
   void at_spawn_continuation(sframe_data *f, sframe_data *p);
-  void at_future_continuation(sframe_data *f);
+  void at_future_continuation(sframe_data *f, sframe_data *p);
 
   // Cilk/future task functions
-  void at_future_finish(sfut_data*);
+  void at_future_finish(sframe_data *f, sframe_data *p, sfut_data* fut);
+
+  void begin_strand(sframe_data *f, sframe_data *p);
 
 private:
+  // Helper function for parallelism creation (spawns + create_future)
+  void create_strand(sframe_data *f);
+
   // Helper function for continuations
   void continuation(sframe_data *f);
+
+  // make the set containing u attached if not already
+  void attachify(node *n);
 
   // Helper function for syncs
   node* binary_join(node *f, // fork node
