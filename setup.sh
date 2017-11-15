5c913c24ed382c98c51ced63c09b5409875ee906
Robert Utterback <robertu13@gmail.com>
2017-11-15 10:35:08 -0600

    Changing setup to compile runtime correctly.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # Script to setup everything, for reproducible results.
+BASE=$(pwd)
+COMPDIR=$BASE/llvm-cilk/bin
 
 # Setup compiler (LLVM)
 #./build-llvm-linux.sh
@@ -9,7 +11,8 @@ mkdir -p build
 
 #git clone https://gitlab.com/wustl-pctg/cilkplus-rts runtime
 cd runtime
+FLAGS="-g -O0 -fcilk-no-inline -fcilktool"
 autoreconf -i
-./configure --prefix=$HOME/devel/futurerd/build
+./configure --prefix=$BASE/build CC=$COMPDIR/clang CXX=$COMPDIR/clang++ CFLAGS=$FLAGS CXXFLAGS=$FLAGS
 make -j
 make install
