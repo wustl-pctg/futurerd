daba677b7745288c424267c2273f04116e92ac91
Robert Utterback <robertu13@gmail.com>
2018-01-05 12:09:41 -0600

    Test script improvements

--- test/basic/run.sh
+++ test/basic/run.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+num_failures=0
+failures=""
 for f in test[0-9] test[0-9][0-9]; do
     echo "---------------------------------------------------------"
     echo "Running $f"
     ./$f
+    rc=$?; if [[ $rc != 0 ]]; then
+               let "num_failures += 1"
+               failures="$failures $f"
+           fi
     echo "---------------------------------------------------------"
 done
+
+if [[ $num_failures == 0 ]]; then
+    echo "All tests passed!"
+else
+   echo "$num_failures test failed: $failures"
+fi
