54d9642e2508f9f7dfaf2199dd2786f8c51b3a2c
Robert Utterback <robertu13@gmail.com>
2018-02-28 10:36:39 -0600

    Bench config changes

--- bench/bench.mk
+++ bench/bench.mk
@@ -5,26 +5,24 @@ INC += -I$(PROJECT_HOME)/src    # for future.hpp
 INC += -I$(BUILD_DIR)/include   # for runtime include
 INC += -I$(BENCH_DIR)           # for util stuff
 RDLIB = $(LIB_DIR)/librd-$(ftype).a
-LIB = $(RDLIB) $(RUNTIME_LIB)
-
-# FUTURE_TYPE is already defined in common.mk, used both by the tool and the
-# user code.  In the user code, when race detection is on, if the tool is compiled
-# to use STRUCTURED_FUTURES, we have to use STRUCTURED_FUTURE bench.  If the
-# tool is compiled to use NONBLOCKING_FUTURES, either construction of bench works. 
-# Thus, adding -DSTRUCTURED_FUTURES is the only valid additional flag.
-# XXX: This is no longer necessary. Just use "make ftype=nonblock" (or ftype=structured)
-# APPFLAGS += -DSTRUCTURED_FUTURES
 
 BASEFLAGS = -fcilkplus -fcilk-no-inline
-REACHFLAGS = -fcilktool
-RDFLAGS = -fsanitize=thread -DRACE_DETECT  $(BAGFLAGS) -fno-omit-frame-pointer
+REACHFLAGS = -fcilktool -DREACH_MAINT
+INSTFLAGS = -fsanitize=thread 
+RDFLAGS = -DRACE_DETECT -fno-omit-frame-pointer
 
 ifeq ($(btype),base)
   APPFLAGS = $(BASEFLAGS)
+  LIB = $(RUNTIME_LIB)
 else ifeq ($(btype),reach)
   APPFLAGS = $(BASEFLAGS) $(REACHFLAGS)
+  LIB = $(RDLIB) $(RUNTIME_LIB:.a=-cilktool.a)
+else ifeq ($(btype),inst)
+  APPFLAGS = $(BASEFLAGS) $(REACHFLAGS) $(INSTFLAGS)
+  LIB = $(RDLIB) $(RUNTIME_LIB:.a=-cilktool.a)
 else ifeq ($(btype),rd)
-  APPFLAGS = $(BASEFLAGS) $(REACHFLAGS) $(RDFLAGS)
+  APPFLAGS = $(BASEFLAGS) $(REACHFLAGS) $(INSTFLAGS) $(RDFLAGS)
+  LIB = $(RDLIB) $(RUNTIME_LIB:.a=-cilktool.a)
 else ifeq ($(btype),all)
   btype = all
 else ifeq ($(btype),) # default value
