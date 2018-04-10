06ff96722602501c5bf7543493313907ee154e75
I-Ting Lee <angelee@adams.cse.wustl.edu>
2018-04-10 00:50:00 -0500

    Adding dedup source code

---
+++ bench/pipeline/dedup/run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+curr=`pwd`
+datadir=$curr/data
+rundir=$curr/build
+
+if [ $# -le 1 ]; then
+echo "Usage ./run.sh <prog> <data size> [output file] where"
+echo "      prog includes: serial, cilk"
+echo "      data size includes simdev, simsmall, simmedium, simlarge, and native."
+exit 0
+fi
+
+dsize=$2
+outfile=$3
+
+if [[ $3 = "" ]]; then
+    outfile="dedup.out"
+fi
+
+if [ $1 = 'serial' ]; then
+    cmd="$rundir/dedup-serial "  
+elif [ $1 = 'cilk' ]; then
+    cmd="$rundir/bt-base "
+fi
+
+cmd+="-c -i $datadir/$dsize/media.dat -o $outfile"
+
+echo "$cmd"
+echo `$cmd`
+
+diffcmd="diff $outfile $datadir/$dsize/dedup.out"
+echo "$diffcmd"
+echo `$diffcmd`
+
