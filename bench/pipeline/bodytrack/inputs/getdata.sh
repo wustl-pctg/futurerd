297e0323c8ec95d29e3f82e19114cdd7b494e1c9
Robert Utterback <robertu13@gmail.com>
2018-11-19 16:03:39 -0600

    script to get data for bodytrack

---
+++ bench/pipeline/bodytrack/inputs/getdata.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# simdev missing? (actually maybe it's there)
+wget http://parsec.cs.princeton.edu/download/3.0/parsec-3.0-input-sim.tar.gz
+wget http://parsec.cs.princeton.edu/download/3.0/parsec-3.0-input-native.tar.gz
+
+# The above gets the inputs, but not the correct outputs if you want
+# to check the results...Need to extract them from this file.
+#http://parsec.cs.princeton.edu/download/3.0/parsec-3.0-core.tar.gz
