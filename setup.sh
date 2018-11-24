e127ae16e2a96ed52e0da96bc6ba115a614fd527
Robert Utterback <robertu13@gmail.com>
2018-11-23 19:39:55 -0600

    setup script must autoreconf runtime

--- setup.sh
+++ setup.sh
@@ -3,19 +3,17 @@
 BASE=$(pwd)
 COMPDIR=$BASE/llvm-cilk/bin
 
+mkdir -p build
+
 # Setup compiler (LLVM)
 ./build-llvm-linux.sh
 
-#git clone https://github.com/wsmoses/Tapir-Meta tapir
-
 # Setup runtime system
-mkdir -p build
-
 git clone -b futurerd https://gitlab.com/wustl-pctg/cilkplus-rts runtime
-# cd runtime
-# autoreconf -i
-# ./remake.sh opt pre
-# cd -
+cd runtime
+autoreconf -i
+# ./remake.sh optlto
+cd -
 
 cd bench/pipeline/dedup/inputs
 wget http://parsec.cs.princeton.edu/download/3.0/parsec-3.0-input-native.tar.gz
